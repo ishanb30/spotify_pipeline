e70bafc11dfe2fec541c2f04b67542df00a5ef5a
Ishan Baddipudi <ish37b@gmail.com>
2026-06-04 16:44:22 +0100

    Set up root logger and handler and configures the formatter

--- utils/logging.py
+++ utils/logging.py
@@ -1,7 +1,19 @@
+"""
+logging.py - sets up root logger and handler and configures the formatter
 
+Assumptions:
+    1. PipelineFormatter exists to handle third-party loggers (like Snowflake's)
+       that don't have run_id in their records (logging format)
+"""
 import logging
 import os
 from utils.paths import LOGS_DIR
+
+class PipelineFormatter(logging.Formatter):
+    def format(self, record):
+        if "run_id" not in record.__dict__:
+            record.run_id = "external"
+        return super().format(record)
 
 def get_logger(name: str, run_id: str) -> logging.LoggerAdapter:
     root = logging.getLogger()
@@ -10,7 +22,7 @@ def get_logger(name: str, run_id: str) -> logging.LoggerAdapter:
         os.makedirs(LOGS_DIR, exist_ok=True)
 
         handler = logging.FileHandler(LOGS_DIR / "pipeline.log")
-        formatter = logging.Formatter(
+        formatter = PipelineFormatter(
             "%(asctime)s - %(run_id)s - %(funcName)s - %(levelname)s - %(message)s"
         )
 
