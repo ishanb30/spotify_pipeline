36b050a8541552500271ba114ae44c273ec38384
Ishan Baddipudi <ish37b@gmail.com>
2026-06-06 08:47:22 +0100

    Organised logic and improved error handling

--- run.py
+++ run.py
@@ -5,70 +5,100 @@ Assumptions:
     1. The table PIPELINE_RUNS exists to be written to
 
 Known Limitations:
-    1. If the UPDATE query in the finally block fails, then RUN_STATUS will
-       not be updated to FAILED in PIPELINE_RUNS. Therefore, it is possible
-       that a failed pipeline run could have a RUN_STATUS of STARTED.
-       However the failure is caught and logged to pipeline.log
+    1. If the UPDATE query in the main except block in run_pipeline fails,
+       then RUN_STATUS will not be updated to FAILED in PIPELINE_RUNS.
+       Therefore, it is possible that a failed pipeline run could have a
+       RUN_STATUS of STARTED. However, the failure is caught and logged to
+       pipeline.log
 """
 
 import uuid
+import sys
 from src.load import load
 from src.fetch import get_api_data
 from utils.logging import get_logger
 from src.connector import get_connection
 from datetime import datetime, timezone
 
-run_id = str(uuid.uuid4())
-logger = get_logger(__name__, run_id)
+def _insert_pipeline_run(cursor, run_id: str, run_status: str):
+    """Inserts run_id and run_status into PIPELINE_RUNS"""
+    run_start = datetime.now(timezone.utc)
 
-watermark = None
-status = "STARTED"
+    cursor.execute(
+        "INSERT INTO SPOTIFY_PIPELINE.RAW.PIPELINE_RUNS (RUN_ID, RUN_START, RUN_STATUS) "
+        "VALUES (%s, %s, %s)",
+        (run_id, run_start, run_status)
+    )
 
-try:
-    logger.info("Pipeline run started")
-    run_start = datetime.now(timezone.utc).replace(tzinfo=None)
+def _update_pipeline_run(
+        cursor, run_id: str, run_status: str, watermark: datetime | None = None
+) -> None:
+    """Updates the PIPELINE_RUNS table once pipeline run status has been determined"""
+    run_end = datetime.now(timezone.utc)
 
-    with get_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(
-                "INSERT INTO SPOTIFY_PIPELINE.RAW.PIPELINE_RUNS (RUN_ID, RUN_START, RUN_STATUS) "
-                "VALUES (%s, %s, %s)",
-                (run_id, run_start, status)
-            )
-            conn.commit()
+    cursor.execute(
+        "UPDATE SPOTIFY_PIPELINE.RAW.PIPELINE_RUNS "
+        "SET RUN_END = %s, WATERMARK_TIMESTAMP = %s, RUN_STATUS = %s "
+        "WHERE RUN_ID = %s",
+        (run_end, watermark, run_status, run_id)
+    )
 
-    data = get_api_data(run_id)
-    watermark = load(run_id, data)
-
-    if watermark:
-        # dbt orchestration goes here
-        status = "COMPLETED"
-        # logger.info("dbt transformation layer complete")
-
-    else:
-        status = "COMPLETED"
-        logger.info("Pipeline exited early - transformation layer skipped")
-
-except Exception as e:
-    status = "FAILED"
-    logger.error(f"{e} | caused by: {e.__cause__}")
-
-finally:
-    run_end = datetime.now(timezone.utc).replace(tzinfo=None)
+def run_pipeline(run_id: str, logger) -> None:
+    """Main execution block for pipeline run logic"""
+    run_status = "STARTED"
 
     try:
         with get_connection() as conn:
             with conn.cursor() as cursor:
-                cursor.execute(
-                    "UPDATE SPOTIFY_PIPELINE.RAW.PIPELINE_RUNS "
-                    "SET RUN_END = %s, WATERMARK_TIMESTAMP = %s, RUN_STATUS = %s "
-                    "WHERE RUN_ID = %s",
-                    (run_end, watermark, status, run_id)
-                )
+                _insert_pipeline_run(cursor, run_id, run_status)
                 conn.commit()
 
-        logger.info(f"Pipeline run finalised with status: {status}")
+                logger.info("Fetching data from API...")
+                data = get_api_data(run_id)
+                logger.info("Loading data to Snowflake...")
+                watermark = load(run_id, data)
 
-    except Exception as meta_e:
-        logger.error(f"Failed to write pipeline metadata to Snowflake: {meta_e}")
+                if watermark:
+                    # logger.info("Transforming data...)
+                    # TODO: trigger dbt transformation layer
+                    pass
+
+                else:
+                    logger.info("Pipeline exited early - transformation layer skipped")
+
+                run_status = "COMPLETED"
+                _update_pipeline_run(cursor, run_id, run_status, watermark)
+                conn.commit()
+                logger.info(f"Pipeline successfully executed")
+
+    except Exception as e:
+        logger.critical(f"Pipeline crashed during execution: {e}", exc_info=True)
+
+        try:
+            with get_connection() as conn:
+                with conn.cursor() as cursor:
+                    run_status = "FAILED"
+                    _update_pipeline_run(cursor, run_id, run_status)
+                    conn.commit()
+                    logger.info("Successfully updated pipeline status to FAILED in Snowflake")
+
+        except Exception as db_err:
+            logger.error(f"Could not log FAILED status to Snowflake: {db_err}")
+
+        raise e
+
+def main():
+    run_id = str(uuid.uuid4())
+    logger = get_logger(__name__, run_id)
+
+    try:
+        logger.info(f"Pipeline initialised")
+        run_pipeline(run_id, logger)
+
+    except Exception as e:
+
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
 
