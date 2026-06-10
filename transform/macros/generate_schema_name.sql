a74dcb9e8f425ab315bcef6e627951923d03bc03
Ishan Baddipudi <ish37b@gmail.com>
2026-06-10 16:50:34 +0100

    Fix generate_schema_name macro signature for dbt 1.11 compatibility

--- transform/macros/generate_schema_name.sql
+++ transform/macros/generate_schema_name.sql
@@ -4,7 +4,7 @@
  back to the target schema from profiles.yml.
 #}
 
-{% macro generate_schema_name(custom_schema_name) %}
+{% macro generate_schema_name(custom_schema_name, node) %}
     {% if custom_schema_name %}
         {{ custom_schema_name }}
     {% else %}
